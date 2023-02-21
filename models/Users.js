986df4a5d6539d949a57a6ae73a4d0c14a678f1b
yairy <yairy6@gmail.com>
2023-02-21 23:06:07 +0200

    add users schema

---
+++ models/Users.js
@@ -0,0 +1,48 @@
+import mongoose from "cmongoose";
+
+export const usersCollectionName = "Users";
+
+export const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        trim: true,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        require: true
+    },
+    firstName:{
+        type: String,
+        trim: true,
+        require: true
+    },
+    lastName:{
+        type: String,
+        trim: true,
+        require: true
+    },
+    phoneNamber:{
+        type: Nubmer,
+        require: true
+    },
+    gender:{
+        type: String,
+        require: true
+    },
+    age:{
+        type: Nubmer,
+        require: true,
+        min: 18,
+        max: 120
+    },
+    creationDate:{
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+export const UsersModel = mongoose.model(usersCollectionName, userSchema);

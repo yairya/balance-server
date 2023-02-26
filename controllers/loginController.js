7f944bfa275ccc3d39ebc7aea39e6987305a06eb
yairy <yairy6@gmail.com>
2023-02-26 23:08:48 +0200

    add create route

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -15,7 +15,7 @@ export const getAllUsers = async(req,res) => {
 
 //-------------------------------------------------------------------------
 export const createUser = async(req,res) => {
-    console.log("create user req camming");
+    console.log("create new user req camming");
     console.log(req.body);
     try{
         console.log("searching after user according email");
